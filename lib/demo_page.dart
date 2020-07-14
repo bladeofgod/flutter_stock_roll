c1400d9904f1ef9bcc2fb977074af833f094bf51
bladeofgods <bladeofgods@163.com>
2020-07-14 12:02:07 +0800

    scroll dev

--- lib/demo_page.dart
+++ lib/demo_page.dart
@@ -16,6 +16,10 @@ class DemoPage extends StatefulWidget{
 
 }
 
+enum SlideDirection{
+  Left,Right,Up,Down
+}
+
 class DemoPageState extends State<DemoPage> {
 
   final Size size;
@@ -28,8 +32,17 @@ class DemoPageState extends State<DemoPage> {
 
   final double blockHeight = 40;
 
+
+  //滑动方向
+  SlideDirection slideDirection;
+
+  //底层横向滚动
   ScrollController rightController;
-  ScrollController stockRowController;
+  //ScrollController stockRowController;
+  //控制左侧股票名称和右侧详情
+  ScrollController stockVerticalController;
+  ScrollController stockNameController;
+
 
   DemoPageState(this.size)
     : miniPageWidth = size.width/4*3,
@@ -43,36 +56,107 @@ class DemoPageState extends State<DemoPage> {
     rightController = ScrollController(
       initialScrollOffset: quarter * 2
     );
-    stockRowController = ScrollController();
+    //stockRowController = ScrollController();
+    stockVerticalController = ScrollController();
+    stockNameController = ScrollController();
 
     super.initState();
+  }
+
+  Offset lastPos;
+
+  handleStart(DragStartDetails details){
+    lastPos = details.globalPosition;
+    logInfo('start', '${details.globalPosition.dx}   ${details.globalPosition.dy}');
+    logInfo('start', '${stockVerticalController.offset}');
+  }
+
+  handleEnd(DragEndDetails details){
+    logInfo('end', 'drag end');
+  }
+
+  handleUpdate(DragUpdateDetails details){
+//    logInfo('update', 'direction   ${details.globalPosition.direction}');
+//    logInfo('update', 'delta ${details.delta}');
+    //logInfo('update', '${details.globalPosition.dx}---${details.globalPosition.dy}');
+    if((details.globalPosition.dx - lastPos.dx).abs() > (details.globalPosition.dy - lastPos.dy).abs()){
+      ///横向滑动
+      if(details.globalPosition.dx > lastPos.dx){
+        //向右
+        slideDirection = SlideDirection.Right;
+      }else{
+        //向左
+        slideDirection = SlideDirection.Left;
+      }
+
+    }else{
+      ///纵向
+      if(details.globalPosition.dy > lastPos.dy){
+        //向下
+        slideDirection = SlideDirection.Down;
+      }else{
+        //向上
+        slideDirection = SlideDirection.Up;
+      }
+    }
+    double dis = (details.globalPosition.dy - lastPos.dy).abs();
+    //logInfo('distance', '$dis');
+    switch(slideDirection){
+
+      case SlideDirection.Left:
+        // TODO: Handle this case.
+        break;
+      case SlideDirection.Right:
+        // TODO: Handle this case.
+        break;
+      case SlideDirection.Up:
+
+        if(stockVerticalController.offset < stockVerticalController.position.maxScrollExtent){
+          stockVerticalController.jumpTo(stockVerticalController.offset+dis);
+          stockNameController.jumpTo(stockNameController.offset+dis);
+        }
+
+        break;
+      case SlideDirection.Down:
+        if(stockVerticalController.offset > stockVerticalController.position.minScrollExtent){
+          stockVerticalController.jumpTo(stockVerticalController.offset-dis);
+          stockNameController.jumpTo(stockNameController.offset-dis);
+        }
+        break;
+    }
+    lastPos = details.globalPosition;
   }
 
 
   @override
   Widget build(BuildContext context) {
-
+    //debugPrint('${MediaQuery.of(context).size}');
     return Material(
-      child: Container(
-        padding: MediaQuery.of(context).padding,
-        color: Colors.white,
-        width: size.width,height: size.height,
-        child: Stack(
-          children: <Widget>[
-            ///bottom part
-            buildBottomPart(size),
-            ///left top
-            Container(
-              color: Colors.white,
-              alignment: Alignment.center,
-              width: quarter,height: blockHeight,
-              child: Text('编辑',style: TextStyle(color: Colors.black),),
-            ),
-            ///left stock name
-            buildStockName(size),
+      child: GestureDetector(
+        onPanStart: handleStart,
+        onPanEnd: handleEnd,
+        onPanUpdate: handleUpdate,
+        child: Container(
+          padding: MediaQuery.of(context).padding,
+          color: Colors.white,
+          width: size.width,height: size.height,
+          child: Stack(
+            children: <Widget>[
+              ///bottom part
+              buildBottomPart(size),
+              ///left top
+              Container(
+                color: Colors.white,
+                alignment: Alignment.center,
+                width: quarter,height: blockHeight,
+                child: Text('编辑',style: TextStyle(color: Colors.black),),
+              ),
+              ///left stock name
+              buildStockName(size),
 
 
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -97,13 +181,7 @@ class DemoPageState extends State<DemoPage> {
             Container(
               width: miniPageWidth,height: size.height,
               color: Colors.blue,
-              child: Stack(
-                children: <Widget>[
-                  Positioned(
-                    child: buildStockDetail(),
-                  ),
-                ],
-              ),
+              child: buildStockDetail(),
             ),
           ],
         ),
@@ -113,6 +191,8 @@ class DemoPageState extends State<DemoPage> {
 
   Widget buildStockDetail(){
     return ListView.builder(
+      controller: stockVerticalController,
+      physics: NeverScrollableScrollPhysics(),
       padding: EdgeInsets.all(0),
       itemCount: 50,
       itemBuilder: (ctx,index){
@@ -146,6 +226,8 @@ class DemoPageState extends State<DemoPage> {
       margin: EdgeInsets.only(top: blockHeight),
       width: quarter,height: size.height - blockHeight,
       child: ListView.builder(
+        controller: stockNameController,
+        physics: NeverScrollableScrollPhysics(),
         padding: EdgeInsets.all(0),
           itemCount: 50,
           itemBuilder: (ctx,index){
@@ -158,6 +240,9 @@ class DemoPageState extends State<DemoPage> {
   }
 
 
+  logInfo(String title,String info){
+    debugPrint('$title ------ $info');
+  }
 
 }
 
